1e6d27e2578a45d4ba0e75fb2f4f28f2da1e4d3a
Nima Abady <nima.abady@gmail.com>
2025-10-02 12:18:15 -0400

    adding a check to see if Logs directory exists and lowering bound for 10 results not 11

--- question3/addFiles.js
+++ question3/addFiles.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdirSync(path.join(__dirname, "./Logs"), {recursive: true});
-for(let i = 0; i <= 10; i++){
+if(!fs.existsSync(path.join(__dirname, "./Logs"))){
+    fs.mkdirSync(path.join(__dirname, "./Logs"), {recursive: true});
+}
+for(let i = 0; i < 10; i++){
     let fileName = "./Logs/logs" + i + ".txt"
     let fileDescription = "this is file " + i
     fs.writeFileSync(path.join(__dirname, fileName), fileDescription, 'utf8')
--- question3/removeFiles.js
+++ question3/removeFiles.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-for(let i = 0; i <= 10; i++){
+for(let i = 0; i < 10; i++){
     let fileName = "./Logs/logs" + i + ".txt"
     if(fs.existsSync(path.join(__dirname, fileName))){
         fs.unlinkSync(path.join(__dirname, fileName))
